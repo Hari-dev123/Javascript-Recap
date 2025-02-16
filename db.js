d4a96eb3c7c6ba7ced6760c107e4e7e2656ce14a
= <=>
2025-02-16 13:17:59 +0530

    feat : update

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 const dbConnection = async()=>{
      try {
           // await mongoose.connect('mongodb://127.0.0.1:27017/rest')
-          await mongoose.connect('mongodb://127.0.0.1:27017/rest')
+          await mongoose.connect('mongodb+srv://mathanhari2:fzSC0xocl95E50Ms@rest.vjshg.mongodb.net/rest?retryWrites=true&w=majority&appName=rest')
           console.log('DB connected')
      } catch (error) {
          console.log(error.message);
